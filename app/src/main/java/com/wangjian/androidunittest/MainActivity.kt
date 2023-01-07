9d49daa3868bac82b9802c5d943e45e79fa57e3c
wangjian <1637299051@qq.com>
2023-01-07 18:13:38 +0800

    添加Espresso部分案列

--- app/src/main/java/com/wangjian/androidunittest/MainActivity.kt
+++ app/src/main/java/com/wangjian/androidunittest/MainActivity.kt
@@ -3,13 +3,14 @@ package com.wangjian.androidunittest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wangjian.androidunittest.espresso.EspressoSimpleActivity
 import com.wangjian.androidunittest.robolectric.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this@MainActivity,WelcomeActivity::class.java))
+        startActivity(Intent(this@MainActivity,EspressoSimpleActivity::class.java))
         finish()
     }
 }
